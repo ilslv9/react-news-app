3be92f4dbd060e6f2dc4210a9870fc85f3ecaede
ilya.suslov <Ilya.Suslov@fors.ru>
2019-12-12 17:51:17 +0300

    Add loading

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import * as serviceWorker from './serviceWorker';
 import NewsList from './components/NewsList';
+
+const loadingStyle = {
+    position: "absolute",
+    top: "50%",
+    marginLeft: "50%",
+    transform: "translate(0, -50%)"
+}
+
+ReactDOM.render(<h1 style={loadingStyle}>Loading...</h1>, document.getElementById('root'))
 
 fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=9fda98e80e7c43bb9f8c20540c95a971')
     .then(response => {
@@ -12,7 +20,7 @@ fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=9fda98e80e7c43bb9f
 
 function renderData(data) {
     console.log(data)
-    ReactDOM.render(<NewsList data={data}/>, document.getElementById('root'));
+    ReactDOM.render(<NewsList data={data} />, document.getElementById('root'));
 }
 
 serviceWorker.unregister();
