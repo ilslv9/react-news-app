87a36d0a4ffa05441df69f0de6ecac06dddb0582
ilya.suslov <Ilya.Suslov@fors.ru>
2019-12-12 16:03:18 +0300

    Fetch data and prepare for list render

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,1 +1,13 @@
 //9fda98e80e7c43bb9f8c20540c95a971
+import React from 'react';
+
+function NewsItem(props) {
+    return (
+        <div>
+            <h1>{props.title}</h1>
+            <h2>{props.text}</h2>
+        </div>
+    );
+}
+
+export default NewsItem;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import NewsItem from './components/NewsItem'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=9fda98e80e7c43bb9f8c20540c95a971')
+    .then(response => {
+        response.json().then(data => console.log(data))
+    })
+    .catch(error => console.log(error))
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(<NewsItem title="Test" text="Another test" />, document.getElementById('root'));
+
+function renderData(data) {
+    
+}
+
 serviceWorker.unregister();
